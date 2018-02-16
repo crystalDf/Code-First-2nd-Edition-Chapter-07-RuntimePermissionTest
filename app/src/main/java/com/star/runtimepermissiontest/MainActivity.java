e2189a0fe46c9a87ba4b62adc54d9a1d17a32bf5
crystalDf <chendong333@gmail.com>
2018-02-16 13:56:53 +0800

    Chapter 07 RuntimePermissionTest

--- app/src/main/java/com/star/runtimepermissiontest/MainActivity.java
+++ app/src/main/java/com/star/runtimepermissiontest/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -24,20 +23,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mMakeCallButton = (Button) findViewById(R.id.make_call);
-        mMakeCallButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        mMakeCallButton = findViewById(R.id.make_call);
+        mMakeCallButton.setOnClickListener(v -> {
 
-                if (ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[] {Manifest.permission.CALL_PHONE}, REQUEST_CODE);
-                } else {
-                    call();
-                }
-
+            if (ContextCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[] {Manifest.permission.CALL_PHONE}, REQUEST_CODE);
+            } else {
+                call();
             }
+
         });
     }
 
